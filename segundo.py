da60eee073df86b4179af1f258d624f6585fee43
Isabel Gonçalves <93488292+bellDataSc@users.noreply.github.com>
2022-02-19 13:25:51 -0300

    Create segundo.py

---
+++ segundo.py
@@ -0,0 +1,12 @@
+#imprimindo valores em uma lista
+
+x = list((1,2,3,4,5))
+x = ['primeiro', 'segundo', 'terceiro', 'quarto', 'quinto', 'penúltimo', 'último']
+
+x[3]
+
+x[-1]
+
+x[2:5]
+
+#imprime: ['terceiro', 'quarto', 'quinto']
