cbd649db55e1b2905f9d2331aee58e08bc117e66
Isabel Gonçalves <93488292+bellDataSc@users.noreply.github.com>
2022-02-19 14:52:13 -0300

    Create quarto.py

---
+++ quarto.py
@@ -0,0 +1,17 @@
+#if elif else
+
+tempo = "chuva"
+
+if tempo=="sol":
+    print("Sair")
+elif tempo=="Chuva":
+    print("Não fiz churrasco")
+elif tempo=="Nublado":
+    print("Nem almocei, fiquei sem fome")
+elif tempo=="Neve":
+    print("Comi fondue")
+else:
+    print("Pedi delivery")
+   
+  
+ 
