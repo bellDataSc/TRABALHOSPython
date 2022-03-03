176fd5a1ad533d2ac8a5e5f4ea9f39c90eaf5014
Isabel Gonçalves <93488292+bellDataSc@users.noreply.github.com>
2022-03-02 23:09:59 -0300

    Create quinto.py

---
+++ quinto.py
@@ -0,0 +1,11 @@
+
+#imprime dados de uma lista
+
+idades= {
+                  'Michael Scott': [43],
+                  'Pam Beesly': [25],
+                  'Jim Halpert': [24]
+                  }
+
+print(f"Michael Scott tem: {(idades['Michael Scott'])} anos de idade.")
+
