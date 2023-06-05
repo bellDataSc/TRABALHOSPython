8e7489d8cc9fafe8b3444d6901098e466cdc47af
Isabel Gonçalves <93488292+bellDataSc@users.noreply.github.com>
2023-06-05 14:02:46 -0300

    Create nona.py
    Leitura de arquivos - Dados

---
+++ nona.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+# Provide the path to your CSV file
+file_path = 
+file
+"arquivo_em.csv"
+
+# Read the CSV file into a DataFrame
+data = pd.read_csv(file_path)
+
+
+data
+# Now you can work with the 'data' DataFrame
+# For example, you can print the first few rows
+print(data.head())
+Fazer"arquivo_em.csv"com
+
+
+
+
+
+
